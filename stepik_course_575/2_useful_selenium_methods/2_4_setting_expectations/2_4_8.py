74b9287106402a4f9e37bd3a0b1c42ee029a8886
AH0HIM <i666943097@gmail.com>
2022-01-17 13:55:18 +0200

    passed

--- stepik_course_575/2_useful_selenium_methods/2_4_setting_expectations/2_4_8.py
+++ stepik_course_575/2_useful_selenium_methods/2_4_setting_expectations/2_4_8.py
@@ -1,19 +1,29 @@
+import math
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+link = "http://suninjuly.github.io/explicit_wait2.html"
 
-link = "http://suninjuly.github.io/wait2.html"
+
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
 
 with webdriver.Chrome() as browser:
     browser.implicitly_wait(5)
 
     browser.get(link)
 
-    button = WebDriverWait(browser, 5).until(
-        EC.element_to_be_clickable((By.ID, "verify"))
-    ).click()
-    message = browser.find_element(By.ID, "verify_message")
+    wait_price = WebDriverWait(browser, 12).until(
+        EC.text_to_be_present_in_element((By.ID, "price"), "100")
+    )
+    browser.find_element(By.ID, "book").click()
+    get_value = browser.find_element(By.ID, "input_value").text
+    browser.find_element(By.ID, "answer").send_keys(calc(get_value))
+    browser.find_element(By.ID, "solve").click()
 
-    assert "successful" in message.text
+    time.sleep(5)
